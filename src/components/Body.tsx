b609c01fd82c4849541c30dcaca0d4aa1271a23b
Yujin Im <jina0605@gmail.com>
2023-04-23 04:00:18 +0900

    Add feature: upload image

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,24 +1,44 @@
 import Image from "next/image";
 import { Settings } from "./Settings";
 import { Canvas } from "./Canvas";
+import { useState } from "react";
 
-export const Body = () => (
-  <div className="flex flex-col items-center space-y-2">
-    <Settings />
-    <div className="flex space-x-4">
-      <button className="bg-green-100 rounded-lg p-2 border border-emerald-500">
-        <p className="font-omyu text-xl leading-none">이어서 병합</p>
-      </button>
-      <button className="bg-green-100 rounded-lg p-2 border border-emerald-500">
-        <p className="font-omyu text-xl leading-none">png 다운로드</p>
-      </button>
-      <button className="bg-green-100 rounded-lg p-2 border border-emerald-500">
-        <p className="font-omyu text-xl leading-none">xnb 다운로드</p>
-      </button>
+export const Body = () => {
+  const [mergeName, setMergeName] = useState("");
+  const [originalName, setOriginalName] = useState("");
+
+  return (
+    <div className="flex flex-col items-center space-y-2">
+      <Settings />
+      <div className="flex space-x-4">
+        <button className="bg-green-100 rounded-lg p-2 border border-emerald-500">
+          <p className="font-omyu text-xl leading-none">이어서 병합</p>
+        </button>
+        <button className="bg-green-100 rounded-lg p-2 border border-emerald-500">
+          <p className="font-omyu text-xl leading-none">png 다운로드</p>
+        </button>
+        <button className="bg-green-100 rounded-lg p-2 border border-emerald-500">
+          <p className="font-omyu text-xl leading-none">xnb 다운로드</p>
+        </button>
+      </div>
+      <div className="flex flex-col md:flex-row md:space-x-6 space-y-2 md:space-y-0 pt-2">
+        <Canvas
+          title={`병합용 파일 ${mergeName}`}
+          img={null}
+          onUpload={(img) => {
+            console.log(img);
+            setMergeName(img.name);
+          }}
+        />
+        <Canvas
+          title={`원본 파일 ${originalName}`}
+          img={null}
+          onUpload={(img) => {
+            console.log(img);
+            setOriginalName(img.name);
+          }}
+        />
+      </div>
     </div>
-    <div className="flex flex-col md:flex-row md:space-x-6 space-y-2 md:space-y-0 pt-2">
-      <Canvas title="병합용 파일" />
-      <Canvas title="원본 파일" />
-    </div>
-  </div>
-);
+  );
+};
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,6 +1,44 @@
-export const Canvas = ({ title }: { title: string }) => (
-  <div className="flex flex-col">
-    <p className="canvas-title">{title}</p>
-    <div className="canvas"></div>
-  </div>
-);
+import Image from "next/image";
+import { useId } from "react";
+
+export interface CanvasProps {
+  title: string;
+  img: any;
+  onUpload: (img: any) => void;
+}
+
+export const Canvas = ({ title, img, onUpload }: CanvasProps) => {
+  const inputId = useId();
+
+  return (
+    <div className="flex flex-col">
+      <p className="canvas-title">{title}</p>
+      <div className="canvas">
+        {img === null && (
+          <>
+            <label htmlFor={inputId}>
+              <div className="w-full h-full flex items-center justify-center cursor-pointer">
+                <Image
+                  src="/assets/icon-plus.svg"
+                  alt="add button"
+                  width="36"
+                  height="36"
+                />
+              </div>
+            </label>
+            <input
+              id={inputId}
+              type="file"
+              name="merge-image"
+              accept="image/png,.xnb"
+              className="hidden"
+              onChange={(e) => {
+                if (e.target.files?.length === 1) onUpload(e.target.files[0]);
+              }}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
