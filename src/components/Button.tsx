e69cef363fe7e71a2c95320f77f8a3bbe4ebc1b0
Yujin Im <jina0605@gmail.com>
2023-04-23 15:49:26 +0900

    Add button hover animation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ export interface ButtonProps {
 }
 
 export const Button = ({ text }: ButtonProps) => (
-  <button className="bg-green-100 rounded-lg p-2 border border-emerald-500">
+  <button className="bg-green-100 rounded-lg p-2 border border-emerald-500 hover:bg-green-200 ease-in duration-100">
     <p className="font-omyu text-xl leading-none">{text}</p>
   </button>
 );
