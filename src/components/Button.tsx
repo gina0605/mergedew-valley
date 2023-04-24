452f9dc2790cc1915ea7ad5b929831c3fb4eca06
Yujin Im <jina0605@gmail.com>
2023-04-25 05:12:14 +0900

    Update button css for disabled

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,11 @@ export interface ButtonProps {
 
 export const Button = ({ text, disabled, onClick }: ButtonProps) => (
   <button
-    className="bg-green-100 rounded-lg p-2 border border-emerald-500 hover:bg-green-200 ease-in duration-100"
+    className={`${
+      disabled
+        ? "bg-slate-50 border-slate-400"
+        : "bg-green-100 hover:bg-green-200 border-emerald-500"
+    } rounded-lg p-2 border ease-in duration-100`}
     disabled={disabled}
     onClick={onClick}
   >
