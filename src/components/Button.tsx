b8d538e730643d3972300b0276654e3f9d77b463
Yujin Im <jina0605@gmail.com>
2023-04-26 04:14:59 +0900

    Update Button css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,10 +10,16 @@ export const Button = ({ text, disabled, onClick }: ButtonProps) => (
       disabled
         ? "bg-slate-50 border-slate-400"
         : "bg-green-100 hover:bg-green-200 border-emerald-500"
-    } rounded-lg p-2 border ease-in duration-100`}
+    } rounded-lg p-2 border ease-in duration-100 w-28`}
     disabled={disabled}
     onClick={onClick}
   >
-    <p className="font-omyu text-xl leading-none">{text}</p>
+    <p
+      className={`font-omyu text-xl leading-none ${
+        disabled ? "text-gray-600" : "text-black"
+      }`}
+    >
+      {text}
+    </p>
   </button>
 );
