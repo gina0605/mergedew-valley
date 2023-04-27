71aa53b370e0e7de80024917d70dd0e46dd6ddff
Yujin Im <jina0605@gmail.com>
2023-04-28 01:40:26 +0900

    Add title tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import { Body } from "@/components/Body";
 import { Header } from "@/components/Header";
 
 const Home = () => (
   <main className="w-full min-h-screen bg-white">
+    <Head>
+      <title>병합듀밸리</title>
+    </Head>
     <Header />
     <Body />
   </main>
