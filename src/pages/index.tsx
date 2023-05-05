1fdc6628d7e13b35d32d0e6a77f0282f1bfd2d84
Yujin Im <jina0605@gmail.com>
2023-05-06 03:46:43 +0900

    Add meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,31 @@ import Head from "next/head";
 import { Body } from "@/components/Body";
 import { Header } from "@/components/Header";
 
-const Home = () => (
-  <main className="w-full min-h-screen bg-white">
-    <Head>
-      <title>병합듀밸리</title>
-    </Head>
-    <Header />
-    <Body />
-    <p className="w-full text-center text-xs text-slate-400 mb-2">
-      mergedew-valley v1.0.0&nbsp;&nbsp;|&nbsp;&nbsp;
-      <a
-        href="https://github.com/gina0605/mergedew-valley"
-        className="underline decoration-1"
-      >
-        Github
-      </a>
-    </p>
-  </main>
-);
+const Home = () => {
+  const seoImg = "https://mergedew-valley.com/seo-img.png";
+
+  return (
+    <main className="w-full min-h-screen bg-white">
+      <Head>
+        <title>병합듀밸리</title>
+        <meta property="og:image" content={seoImg} />
+        <meta name="twitter:title" content="병합듀밸리" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" content={seoImg} />
+      </Head>
+      <Header />
+      <Body />
+      <p className="w-full text-center text-xs text-slate-400 mb-2">
+        mergedew-valley v1.0.0&nbsp;&nbsp;|&nbsp;&nbsp;
+        <a
+          href="https://github.com/gina0605/mergedew-valley"
+          className="underline decoration-1"
+        >
+          Github
+        </a>
+      </p>
+    </main>
+  );
+};
 
 export default Home;
